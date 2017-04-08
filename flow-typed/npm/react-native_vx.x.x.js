731356275441da49beb2be528a8d8718fd0fd574
Ty Cobb <ty.cobb.m@gmail.com>
2017-04-08 14:16:51 -0500

    add flow

---
+++ flow-typed/npm/react-native_vx.x.x.js
@@ -0,0 +1,6 @@
+// flow-typed signature: 62b46b92c81450faa061de14ba887353
+// flow-typed version: <<STUB>>/react-native_v0.43.2/flow_v0.38.0
+
+declare module 'react-native' {
+  declare module.exports: any;
+}
---
+++ flow-typed/npm/react-relay_vx.x.x.js
@@ -0,0 +1,6 @@
+// flow-typed signature: e3d8d926fbeb3a0332dd038383be245d
+// flow-typed version: <<STUB>>/react-relay_v0.10.0/flow_v0.38.0
+
+declare module 'react-relay' {
+  declare module.exports: any;
+}
